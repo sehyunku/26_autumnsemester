32e59b9b55312b452ba83592d2bd253ee274d65c
sehyunku <sehyunku@ewha.ac.kr>
2026-09-07 04:29:04 +0000

    yes!

--- main.cpp
+++ main.cpp
@@ -1,7 +1,17 @@
 
 
 #include <iostream>
+#include <typeinfo>
+
 int main()
 {
+    int numOfStudents{30};
+    std::cout << "Enter the number of students: ";
+    std::cin >> numOfStudents;
+    std::cout << numOfStudents << std::endl;
+    std::cout << sizeof(numOfStudents) << std::endl;
+    std::cout << typeid(numOfStudents).name() << std::endl;
+    static_cast<double>(numOfStudents);
+    std::cout << typeid(static_cast<double>(numOfStudents)).name() << std::endl;
     return 0;
 }
